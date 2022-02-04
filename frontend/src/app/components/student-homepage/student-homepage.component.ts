90ca21a59c89e5bf78fd7ae26cfcba875665dcce
Dibyendu Saha <dip4012@gmail.com>
2022-02-05 04:31:08 +0530

    Added a cool INDETERMINATE PROGRESS BAR to the student login page

--- frontend/src/app/components/student-homepage/student-homepage.component.ts
+++ frontend/src/app/components/student-homepage/student-homepage.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-student-homepage',
@@ -8,13 +9,27 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class StudentHomepageComponent implements OnInit {
   loginForm!: FormGroup;
+  hide = true;
 
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private fb: FormBuilder,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.loginForm = this.fb.group({
       email: ['', Validators.required],
       password: ['', Validators.required],
     });
+  }
+
+  toRegister() {
+    this.hide = false;
+    setTimeout(
+      () =>
+        this.router.navigate(['register'], { relativeTo: this.activatedRoute }),
+      2000
+    );
   }
 }
